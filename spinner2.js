3965923d85770e8d4a17753bb3972584b5a4f175
BlesMateo <blesildaangelicamateo@gmail.com>
2021-11-02 19:24:39 -0400

    Refactored the original file to remove the repeat setTimeout statements

---
+++ spinner2.js
@@ -0,0 +1,59 @@
+/*
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 1100);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 1300);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 1500);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 1700);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 1900);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 2100);
+*/
+
+let start = 100;
+const lines = ["|", "/", "-", "\\", "|", "/", "-", "\\"];
+for (const line of lines) {
+  setTimeout(() => {
+      process.stdout.write(`\r${line}  `);
+  }, start);
+
+  start = start + 200;
+}
+
+setTimeout(() => {
+  process.stdout.write("\n");
+}, start);
